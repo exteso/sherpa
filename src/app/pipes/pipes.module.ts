b576c07fadf90094b95e804174d5762f8a6afc71
Federico Yankelevich <yankedev@exteso.com>
2020-03-05 23:48:16 +0100

    cleanup

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { UsersPipe } from './users.pipe';
-import { SearchPipe } from './search.pipe';
-import { SearchProductPipe } from './search-product.pipe';
 
 // Add your pipes here for easy indexing.
 @NgModule({
   declarations: [
-    UsersPipe,
-    SearchPipe,
-    SearchProductPipe
+    UsersPipe
   ],
   imports: [
     IonicModule
   ],
   exports: [
-    UsersPipe,
-    SearchPipe,
-    SearchProductPipe
+    UsersPipe
   ]
 })
 
