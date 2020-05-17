8906eb8ae4d462a5e0f2a859152a6363b79b7bda
Federico Yankelevich (yankedev) <yankedev@exteso.com>
2020-05-18 00:06:17 +0200

    feat(collect-order): insert real pricenow you can directly set the price and realQty is calculated and stored.I've been playing with rounding numbers and simple pattern matchingfor fractions  as '2/7'.

--- src/app/pages/modal/correct-real-quantity/correct-real-quantity.page.ts
+++ src/app/pages/modal/correct-real-quantity/correct-real-quantity.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Grocery } from 'src/app/models/grocery';
 import { ModalController } from '@ionic/angular';
 import { DecimalPipe } from '@angular/common';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-correct-real-quantity',
@@ -61,6 +62,55 @@ export class CorrectRealQuantityPage implements OnInit {
 
     let price = this.realQty * this.item.price;
     let priceText = this.decimalPipe.transform(price, '1.2-2')
-    return priceText + " CHF"
+    return priceText;
+  }
+
+  onChangeRealPrice(realPrice: number){
+    let calcQty = realPrice / this.item.price;
+    this.realQty = this.formatNumber(calcQty, 4);
+  }
+
+  onChangeRealQty(qty: number|string){
+    let realQty = 0;
+    if (typeof qty === "string"){
+      if (qty.indexOf('/') > 0){
+        let nums = qty.split('/');
+        realQty = parseFloat(nums[0]) / parseFloat(nums[1]);
+      } else {
+        realQty = parseFloat(qty);
+      }
+    } else {
+      realQty = qty;
+    }
+
+    if (realQty > 0){
+      this.realQty = parseFloat(this.formatNumber(realQty, 4));
+    }
+  }
+
+  /**
+   * Custom JavaScript function that rounds a number w/
+   * decimal places.
+   *
+   * @param val - The value that you want to format with decimal places.
+   * @param decimals - The number of decimal places that should be used.
+   * @returns {float}
+   */
+  formatNumber(val, decimals){
+    //Parse the value as a float value
+    val = parseFloat(val);
+    //Format the value w/ the specified number
+    //of decimal places and return it.
+    return val.toFixed(decimals);
+  }
+
+  validateNumber(event) {
+    var key = window.event ? event.keyCode : event.which;
+
+    if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode == 46 || event.keyCode == 47) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
